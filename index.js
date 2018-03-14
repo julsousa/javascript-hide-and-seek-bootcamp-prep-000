8dc09594acafc98d0e3d9d5df56c56448f6b0a59
Julie Sousa <juliesousa@yahoo.com>
2018-03-14 19:45:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function nestedTarget() {
 function increaseRankBy(n) {
   var ranks = document.querySelectorAll('ul.ranked-list li');
   for (var i = 0; i < ranks.length; i++) {
-    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n
+    ranks[i].innerHTML = parseInt(ranks[i].innerHTML) + n;
   } 
 }
 
